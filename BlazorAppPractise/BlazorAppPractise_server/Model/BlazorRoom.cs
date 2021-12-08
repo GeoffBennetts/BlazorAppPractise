3af2f0d1cd5166bbf48ad752c80924426a2b8c02
Geoff Taha <geoff.taha@bennetts.co.uk>
2021-12-08 14:36:12 +0000

    added BlazorAppPractise project

---
+++ BlazorAppPractise/BlazorAppPractise_server/Model/BlazorRoom.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAppPractise_server.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+        public string  RoomName { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
