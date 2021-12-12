c12727eb113225468e565617e2aad1e1eaa13468
Geoff Taha <geoff.taha@bennetts.co.uk>
2021-12-12 20:47:53 +0000

    Added hotel amenities

---
+++ BlazorAppPractise/BlazorAppPractise_server/Model/BlazorHotelAmenities.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppPractise_server.Model
+{
+    public class BlazorHotelAmenities
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- BlazorAppPractise/BlazorAppPractise_server/Model/BlazorRoom.cs
+++ BlazorAppPractise/BlazorAppPractise_server/Model/BlazorRoom.cs
@@ -3,7 +3,7 @@
     public class BlazorRoom
     {
         public int Id { get; set; }
-        public string  RoomName { get; set; }
+        public string?  RoomName { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
         public List<BlazorRoomProp> RoomProps { get; set; }
